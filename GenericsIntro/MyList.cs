02d609f3125139168d2fde5140575ed9c9fbb4a0
ynssynk <ynssynk@hotmail.com>
2021-01-20 23:50:42 +0300

    20 Ocak ders sonu

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace GenericsIntro
+{
+    public class MyList<T>
+    {
+        private T[] items;
+
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        }
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OOP1
+{
+    public class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName+" eklendi.");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi.");
+        }
+    }
+}
